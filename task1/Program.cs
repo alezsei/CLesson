75c29f7e46abb5bd11481b04b64b8dfc11518932
alezsei <alezsei@yandex.ru>
2023-05-21 11:45:09 +0400

    1Taskupgratefinishend

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,14 +4,14 @@ Console.WriteLine("Введите 2 число");
 int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1 > num2)
 {
-    Console.Write($"Большее из дву чисел {num1}");
+    Console.Write($"Большее из дву чисел {num1}, меньшее {num2}");
 }
 else
 
     if (num2 > num1)
 
     {
-        Console.Write($"Большее из дву чисел {num2}");
+        Console.Write($"Большее из дву чисел {num2}, меньшее {num1}");
     }
 
 else 
